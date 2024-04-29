45bc913504223c6375a3acf71a112d3ef24fadf9
BramVandenbossche <bramvandenbossche@hotmail.com>
2024-04-29 22:03:38 +0200

    backup script refactor logging change

--- components/backup/backup.sh
+++ components/backup/backup.sh
@@ -13,51 +13,35 @@ messages=()
 # Functions
 echo_message() {
     local message="$1"
-    local componentname="backup"
-    local error="$2"
-    local timestamp=$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")
-
-    echo '{"timestamp": "'"$timestamp"'","componentName": "'"$componentname"'","message": "'"$message"'","error": '$error'}'
+    echo '{"message": "'"$message"'"}'
 }
 
 end_script(){
-    local status="$1"
-
-    for ((i=0; i<${#messages[@]}; i++)); do
-        echo "${messages[i]}"
-    done
-
     exit $status
 }
 
 # Run
 if [[ -z $VM_CT_ID ]]; then
-    messages+=("$(echo_message "Please provide the VM or CT ID." true)")
+    >&2 echo "Please provide the VM or CT ID."
     end_script 1
 fi
 
 if [[ -z $PBS_STORAGE ]]; then
-    messages+=("$(echo_message "Please specify the PBS storage name." true)")
+    >&2 echo "Please specify the PBS storage name."
     end_script 1
 fi
 
 if [[ -z $SSH_PRIVATE_KEY ]]; then
-    messages+=("$(echo_message "Please provide the SSH private key file name.${NC}" true)")
+    >&2 echo "Please provide the SSH private key file name.${NC}"
     end_script 1
 fi
 
 if [[ -z $PROXMOX_HOST ]]; then
-    messages+=("$(echo_message "Please specify the Proxmox host IP address or hostname." true)")
+    >&2 echo "Please specify the Proxmox host IP address or hostname."
     end_script 1
 fi
 
-backup_output=$(ssh -i "$SSH_PRIVATE_KEY" -o StrictHostKeyChecking=no "$USER"@"$PROXMOX_HOST" "vzdump $VM_CT_ID --storage=\"$PBS_STORAGE\" 2>&1")
-if echo "$backup_output" | grep -iq "error"; then
-    messages+=("$(echo_message "Backup process failed. Error: $backup_output" true)")
-    end_script 1
-else
-    messages+=("$(echo_message "Backup process completed." false)")
-    end_script 0
-fi
+ssh -i "$SSH_PRIVATE_KEY" -o StrictHostKeyChecking=no "$USER"@"$PROXMOX_HOST" "vzdump $VM_CT_ID --storage=\"$PBS_STORAGE\""
+
 
 
