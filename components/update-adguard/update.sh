75367b3abeda9d011d0a52b750d1466a6ef61e29
BramVandenbossche <bramvandenbossche@hotmail.com>
2024-04-30 18:07:06 +0200

    Refactor Adguard component

--- components/update-adguard/update.sh
+++ components/update-adguard/update.sh
@@ -6,29 +6,7 @@ PROXMOX_HOST="$2"
 USER="$3"
 SSH_PRIVATE_KEY="${4:-id_rsa}"
 
-# Vars
-messages=()
-
 # Functions
-echo_message() {
-  local message="$1"
-  local error="$2"
-  local componentname="update-adguard"
-  local timestamp=$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")
-
-  echo '{"timestamp": "'"$timestamp"'","componentName": "'"$componentname"'","message": "'"$message"'","error": '$error'}'
-}
-
-end_script() {
-  local status="$1"
-
-  for ((i = 0; i < ${#messages[@]}; i++)); do
-    echo "${messages[i]}"
-  done
-
-  exit $status
-}
-
 execute_command_on_container() {
   local command="$1"
 
@@ -36,47 +14,43 @@ execute_command_on_container() {
   local exit_status=$?
 
   if [[ $exit_status -ne 0 ]]; then
-    messages+=("$(echo_message "Error executing command on container ($exit_status): $command" true)")
-    end_script 1
-  else
-    while IFS= read -r line; do
-      messages+=("$(echo_message "$line" false)")
-    done <<< "$pct_exec_output"
+    >&2 echo  "Error executing command on container ($exit_status): $command"
+    exit 1
   fi
 }
 
 update() {
   check_output=$(execute_command_on_container "[ -d /opt/AdGuardHome ] && echo 'Installed' || echo 'NotInstalled'")
   if [[ $check_output == "NotInstalled" ]]; then
-    messages+=("$(echo_message "No AdGuardHome Installation Found!" true)")
+    >&2 echo  "No AdGuardHome Installation Found!"
     end_script 1
   fi
 
-  messages+=("$(echo_message "Downloading AdGuardHome" false)")
+  echo "Downloading AdGuardHome"
   execute_command_on_container "wget -qL https://static.adguard.com/adguardhome/release/AdGuardHome_linux_amd64.tar.gz"
 
-  messages+=("$(echo_message "Stopping AdguardHome" false)")
+  echo "Stopping AdguardHome"
   execute_command_on_container "systemctl stop AdGuardHome"
-  messages+=("$(echo_message "Stopped AdguardHome" false)")
+  echo "Stopped AdguardHome"
 
-  messages+=("$(echo_message "Updating AdguardHome" false)")
+  echo "Updating AdguardHome"
   execute_command_on_container "tar -xvf AdGuardHome_linux_amd64.tar.gz &>/dev/null"
   execute_command_on_container "mkdir -p adguard-backup"
   execute_command_on_container "cp -r /opt/AdGuardHome/AdGuardHome.yaml /opt/AdGuardHome/data adguard-backup/"
   execute_command_on_container "cp AdGuardHome/AdGuardHome /opt/AdGuardHome/AdGuardHome"
   execute_command_on_container " cp -r adguard-backup/* /opt/AdGuardHome/"
-  messages+=("$(echo_message "Updated AdguardHome" false)")
+  echo "Updated AdguardHome"
 
-  messages+=("$(echo_message "Starting AdguardHome" false)")
+  echo "Starting AdguardHome"
   execute_command_on_container "systemctl start AdGuardHome"
-  messages+=("$(echo_message "Started AdguardHome" false)")
+  echo "Started AdguardHome"
 
-  messages+=("$(echo_message "Cleaning Up" false)")
+  echo "Cleaning Up"
   execute_command_on_container "rm -rf AdGuardHome_linux_amd64.tar.gz AdGuardHome /opt/AdGuardHome/adguard-backup"
-  messages+=("$(echo_message "Cleaned" false)")
-  messages+=("$(echo_message "Updated Successfully" false)")
+  echo "Cleaned"
+  echo "Updated Successfully"
 }
 
 # Run
 update
-end_script 0
+exit 0
