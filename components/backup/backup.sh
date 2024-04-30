d58c1c55900a9d5869c7cd45586ab18b0d5e4976
BramVandenbossche <bramvandenbossche@hotmail.com>
2024-04-30 18:00:29 +0200

    refactor restore script & errorhandling backup.sh

--- components/backup/backup.sh
+++ components/backup/backup.sh
@@ -10,22 +10,31 @@ SSH_PRIVATE_KEY="${5:-id_rsa}"
 # Run
 if [[ -z $VM_CT_ID ]]; then
     >&2 echo "Please provide the VM or CT ID."
-    exit
+    exit 1
 fi
 
 if [[ -z $PBS_STORAGE ]]; then
     >&2 echo "Please specify the PBS storage name."
-    exit
+    exit 1
 fi
 
 if [[ -z $SSH_PRIVATE_KEY ]]; then
     >&2 echo "Please provide the SSH private key file name.${NC}"
-    exit
+    exit 1
 fi
 
 if [[ -z $PROXMOX_HOST ]]; then
     >&2 echo "Please specify the Proxmox host IP address or hostname."
-    exit
+    exit 1
 fi
 
-ssh -i "$SSH_PRIVATE_KEY" "$USER"@"$PROXMOX_HOST" "vzdump $VM_CT_ID --storage=\"$PBS_STORAGE\""
+ssh_output=$(ssh -i "$SSH_PRIVATE_KEY" "$USER"@"$PROXMOX_HOST" "vzdump $VM_CT_ID --storage=\"$PBS_STORAGE\"" 2>&1)
+ssh_exit_status=$?
+if [[ $ssh_exit_status -ne 0 ]]; then
+    >&2 echo "Failed to execute vzdump command over SSH. Error: $ssh_output"
+    exit 1
+else
+    >&2 echo "vzdump command executed successfully."
+fi
+
+exit 0
