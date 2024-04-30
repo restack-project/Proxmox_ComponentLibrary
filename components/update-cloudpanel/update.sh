c23a800e4f11d4de04f651cc353b4b490f4bfc2c
BramVandenbossche <bramvandenbossche@hotmail.com>
2024-05-01 00:10:12 +0200

    components non proxmox lxc support

--- components/update-cloudpanel/update.sh
+++ components/update-cloudpanel/update.sh
@@ -7,23 +7,27 @@ USER="$3"
 SSH_PRIVATE_KEY="${4:-id_rsa}"
 
 # Functions
-execute_command_on_container() {
+execute_command_on_machine() {
   local command="$1"
 
-  pct_exec_output=$(ssh -i "$SSH_PRIVATE_KEY" -o StrictHostKeyChecking=no "$USER"@"$PROXMOX_HOST" "pct exec $VM_CT_ID -- bash -c '$command' 2>&1")
-  
-  echo "$pct_exec_output"
-  
+  if [[ $VM_CT_ID == "0" || $VM_CT_ID -eq 0 ]]; then
+    output=$(ssh -i "$SSH_PRIVATE_KEY" -o StrictHostKeyChecking=no "$USER"@"$PROXMOX_HOST" "bash -c '$command' 2>&1")
+  else
+    output=$(ssh -i "$SSH_PRIVATE_KEY" -o StrictHostKeyChecking=no "$USER"@"$PROXMOX_HOST" "pct exec $VM_CT_ID -- bash -c \"$command\" 2>&1")
+  fi
+
+  echo "$output"
+
   local exit_status=$?
   if [[ $exit_status -ne 0 ]]; then
-    >&2 echo  "Error executing command on container ($exit_status): $command"
+    >&2 echo "Error executing command on machine ($exit_status): $command"
     exit 1
   fi
 }
 
 update() {
   echo "Updating Cloudpanel"
-  execute_command_on_container "clp-update"
+  execute_command_on_machine "clp-update"
 }
 
 # Run
--- components/update-uptimekuma/update.sh
+++ components/update-uptimekuma/update.sh
@@ -10,22 +10,27 @@ SSH_PRIVATE_KEY="${4:-id_rsa}"
 messages=()
 
 # Functions
-execute_command_on_container() {
+execute_command_on_machine() {
   local command="$1"
 
-  pct_exec_output=$(ssh -i "$SSH_PRIVATE_KEY" -o StrictHostKeyChecking=no "$USER"@"$PROXMOX_HOST" "pct exec $VM_CT_ID -- bash -c '$command' 2>&1")
-  local exit_status=$?
-  
-  echo "$pct_exec_output"
+  if [[ $VM_CT_ID == "0" || $VM_CT_ID -eq 0 ]]; then
+    output=$(ssh -i "$SSH_PRIVATE_KEY" -o StrictHostKeyChecking=no "$USER"@"$PROXMOX_HOST" "bash -c '$command' 2>&1")
+  else
+    output=$(ssh -i "$SSH_PRIVATE_KEY" -o StrictHostKeyChecking=no "$USER"@"$PROXMOX_HOST" "pct exec $VM_CT_ID -- bash -c \"$command\" 2>&1")
+  fi
 
+  echo "$output"
+
+  local exit_status=$?
   if [[ $exit_status -ne 0 ]]; then
-    >&2 echo  "Error executing command on container ($exit_status): $command"
+    >&2 echo "Error executing command on machine ($exit_status): $command"
     exit 1
   fi
 }
 
+
 update() {
-  check_output=$(execute_command_on_container "[ -d /opt/uptime-kuma ] && echo 'Installed' || echo 'NotInstalled'")
+  check_output=$(execute_command_on_machine "[ -d /opt/uptime-kuma ] && echo 'Installed' || echo 'NotInstalled'")
   if [[ $check_output == "NotInstalled" ]]; then
     >&2 echo "No UptimeKuma Installation Found!"
     exit 1
@@ -33,20 +38,20 @@ update() {
 
   LATEST=$(curl -sL https://api.github.com/repos/louislam/uptime-kuma/releases/latest | grep '"tag_name":' | cut -d'"' -f4)
   echo "Stopping Kuma"
-  execute_command_on_container "sudo systemctl stop uptime-kuma"
+  execute_command_on_machine "sudo systemctl stop uptime-kuma"
 
-  execute_command_on_container "cd /opt/uptime-kuma && \
+  execute_command_on_machine "cd /opt/uptime-kuma && \
                                 git fetch --all && \
                                 git checkout $LATEST --force"
   
 
   echo "Updating Kuma to ${LATEST}"
-  execute_command_on_container "cd /opt/uptime-kuma && \
+  execute_command_on_machine "cd /opt/uptime-kuma && \
                                 npm install --production && \
                                 npm run download-dist"
   
   echo "Starting Kuma"
-  execute_command_on_container "sudo systemctl start uptime-kuma"
+  execute_command_on_machine "sudo systemctl start uptime-kuma"
 }
 
 ## Run
